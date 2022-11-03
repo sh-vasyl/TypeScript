24e15666fbffd4bd2e3835e79022eddc13ec18c6
Vasyl <up.vasyl@gmail.com>
2022-11-03 21:29:53 +0200

    never type

--- basics/src/main.ts
+++ basics/src/main.ts
@@ -1,11 +1,7 @@
 import './style.css'
 
-let userInput: unknown
-let userName: string
-
-userInput = 5
-userInput = 'Max'
-
-if (typeof userInput === 'string') {
-	userName = userInput
+function generateError(message: string, code: number): never {
+	throw { message: message, errorCode: code }
 }
+
+generateError('An error', 500)
